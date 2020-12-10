4177f086d64698aebe9478c51b37451d999119bb
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-10 19:06:29 +0530

    moved verifyToken function to its respective module

---
+++ routes/verifyToken.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = async (req, res, next) => {
+  const bearerHeader = req.headers["authorization"];
+  const token = req.cookies.auth;
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    jwt.verify(bearerToken, process.env.JWT_SECRET, (err, data) => {
+      if (err) {
+        return res, status(403).send("Error");
+      }
+      req.user_data = data;
+      next();
+    });
+  } else if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+      if (err) {
+        return res, status(403).send("Error");
+      }
+      req.user_data = data;
+      next();
+    });
+  } else {
+    return res.status(401);
+  }
+};
+
+module.exports = verifyToken;
