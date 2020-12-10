e2683967e1b5c52907fb85cae6f88e4867b17d2a
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-10 18:04:26 +0530

    added routes for signup, login

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const userController = require("../controllers/userController");
 
-router.get('/', (req, res, next) => {
-  res.send('<h1>HOME</h1>')
-})
+router.get("/", (req, res, next) => {
+  res.send("<h1>HOME</h1>");
+});
 
-module.exports = router
+router.post("/signup", userController.signup);
+
+router.post("/login", userController.login);
+
+module.exports = router;
