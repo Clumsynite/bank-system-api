7d8fcf4e765ecc6c75454754739b590609c159eb
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-10 19:23:32 +0530

    improved erro return

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -3,9 +3,8 @@ const accountQueries = require("../db/accountQueries.js");
 exports.allAccounts = async (req, res) => {
   try {
     const allAcounts = await accountQueries.allAccounts();
-    console.log(allAcounts);
     res.json(allAcounts);
   } catch (error) {
-    return res.status(500).json({ error });
+    return res.json({ error });
   }
 };
