9a92971cf70bf44959cba123d1b5c4d3988f9aee
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-10 18:04:39 +0530

    Create userQueries.js
    to add userQueries

---
+++ db/userQueries.js
@@ -0,0 +1,50 @@
+const { column, whereExists } = require("../db/initialise");
+const knex = require("../db/initialise");
+
+exports.insertUser = (user) => {
+  knex("users")
+    .insert(user)
+    .then((data) => {
+      return knex.from("users").select("*").where("username", user.username);
+    })
+    .catch((err) => {
+      console.log(err);
+      return err;
+    });
+};
+
+exports.findByUsername = async (username) => {
+  return await knex
+    .from("users")
+    .select("*")
+    .where("username", username)
+    .then((result) => result)
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
+};
+
+exports.usernameExists = async (username) => {
+  return await knex
+    .from("users")
+    .select("username")
+    .where("username", username)
+    .then((result) => result.length > 0)
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
+};
+
+exports.findByUsernameAndPassword = async (user) => {
+  return await knex
+    .from("users")
+    .select("username", "password")
+    .where(user)
+    .then((result) => result)
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
+};
