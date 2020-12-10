d7af884519ca5e23cf987abe79424f49ef50dd96
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-10 20:18:17 +0530

    added viewTransaction route and control
    added cash withdrawal, get total and transaction by username queries

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,10 +1,35 @@
 const accountQueries = require("../db/accountQueries.js");
+const knex = require("../db/init.js");
 
 exports.allAccounts = async (req, res) => {
   try {
     const allAcounts = await accountQueries.allAccounts();
     res.json(allAcounts);
   } catch (error) {
-    return res.json({ error });
+    return res.json({ msg: "Error viewing accounts" });
+  }
+};
+
+exports.viewTransactions = async (req, res) => {
+  try {
+    return await knex
+      .from("accounts")
+      .join("users", { "accounts.user_id": "users.user_id" })
+      .select("*")
+      .where(username);
+  } catch (error) {
+    return res.json({ msg: "Error viewing Transaction" });
+  }
+};
+
+exports.withdraw = async (req, res) => {
+  try {
+    accountQueries.cashWithdrawal({
+      user_id: req.user.user_id.toString(),
+      cash_withdrawn: Number(req.body.amount),
+    });
+    res.json(req.user);
+  } catch (error) {
+    return res.json({ msg: "Error logging Withdrawal" });
   }
 };
--- db/accountQueries.js
+++ db/accountQueries.js
@@ -8,3 +8,28 @@ exports.allAccounts = async () => {
     return error;
   }
 };
+
+exports.viewTransactionByUsername = async (username) => {
+  try {
+    return await knex.from("users").select("*").where(username);
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+exports.cashWithdrawal = async (transaction) => {
+  try {
+    return knex("accounts").insert(transaction);
+  } catch (error) {
+    return error;
+  }
+};
+
+exports.getUserTotal = async (username) => {
+  try {
+    return await knex.from("accounts").select("total").where(username);
+  } catch (error) {
+    return error;
+  }
+};
